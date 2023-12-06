46f03f79f6b39e06ecd0afb096cf0eeaa906e0d2
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2023-12-07 02:32:48 +0300

    method splitTextIntoWords

--- src/main/java/practice/regex/SplitText.java
+++ src/main/java/practice/regex/SplitText.java
@@ -8,7 +8,13 @@ public class SplitText {
 
   public static String splitTextIntoWords(String text) {
     //TODO реализуйте метод
-    return "";
+    StringBuilder result = new StringBuilder();
+    String regex = "[^A-Za-z\\s]";
+    text = text.replaceAll(regex, "");
+    String[] words = text.split("\\s+");
+    for (int i = 0; i < words.length; i++) {
+      result.append(words[i]).append(System.lineSeparator());
+    }
+    return result.toString().strip();
   }
-
 }
